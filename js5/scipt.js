7f2eae55819628015be2a95c99c91e68e20ffa39
giosaginashvili <164656337+giosaginashvili@users.noreply.github.com>
2024-07-01 03:19:38 -0700

    Add files via upload

---
+++ js5/scipt.js
@@ -0,0 +1,30 @@
+function sayhello (){
+    console.log("hello world")
+}
+
+sayhello()
+
+
+
+
+
+
+function greet (name){
+    console.log("helo" + name)}
+
+    greet(" giorgi")
+
+
+
+    function check_number(num){
+        if(num>10){
+            console.log("num metia atze")
+        }
+else {
+    console.log("atze naklebia")
+}
+
+    }
+
+
+    check_number(9)
